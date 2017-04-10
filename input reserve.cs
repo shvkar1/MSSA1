17a28ae98b9da31245c20c074378cc12a0ac415c
shvkar1 <Shiva.karki1@outlook.com>
2017-04-10 12:16:38 -0400

    Add files via upload

---
+++ input reserve.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace forgetCode
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the string to reverse :");
+            string name = Console.ReadLine();
+ 
+            char[] cArray = name.ToCharArray();
+            string nameReverse = String.Empty;
+            for (int i = cArray.Length - 1; i > -1; i--)
+            {
+                nameReverse += cArray[i];
+            }
+            Console.WriteLine(nameReverse);
+            
+        }
+    }
+}
+
+      
